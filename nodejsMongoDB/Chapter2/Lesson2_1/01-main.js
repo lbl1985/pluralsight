5b83e91bb9cb75f6d923c24e81c0ef591e2b321e
Binlong Li <bil@microsoft.com>
2017-01-29 22:38:35 -0800

    Finish for chapter 2 video 1.

---
+++ nodejsMongoDB/Chapter2/Lesson2_1/01-main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const fs = require('fs');
+const helloString = require('./02-hello-string');
+const helloFunc = require('./02-hello-function');
+const helloObj = require('./04-hello-object');
+const name = require('./04-hello-object').name;
+
+
+// console.log(helloString);
+// helloFunc();
+// helloObj.say('Binlong');
+console.log(name);
