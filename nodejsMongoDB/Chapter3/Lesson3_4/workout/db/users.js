383f9a6bfb89a296ed340d765c7add07c41a7d9a
Binlong Li <bil@microsoft.com>
2017-02-05 23:17:07 -0800

    sending error messages into the jade view

---
+++ nodejsMongoDB/Chapter3/Lesson3_4/workout/db/users.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const records = [
+    {id: 1, username: 'Binlong', password: 'secret', role: 'admin'},
+    {id: 2, username: 'Cindy', password: 'forgot', role:'user'}
+];
+
+exports.findById = function(id, callback) {
+    process.nextTick(function(){
+        const idx = id - 1;
+        if(records[idx] && id == records[idx].id){
+            callback(null, records[idx]);
+        } else {
+            callback(new Error('User ' + id + ' does not exist'));
+        }
+    });
+}
+
+exports.findByUsername = function(username, callback) {
+    process.nextTick(function(){
+        for(let i = 0, lenght = records.length; i < length; i++) {
+            let record = records[i];
+
+            if(username == record.username) {
+                return callback(null, record);
+            } 
+            return callback(null, null);
+        }
+    })
+}
---
+++ nodejsMongoDB/Chapter3/Lesson3_4/workout/routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('../lib/passport');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.route('/login')
+  .get(function(req, res){
+    res.render('login', {error:req.flash('error')});
+  })
+  .post(passport.authenticate('local', {
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
+
+router.get('/logout', function(req, res){
+  req.logout();
+
+  res.redirect('/');
+})
+
+router.get('/profile', function(req, res){
+  res.send('Hello ' + req.user.username);
+})
+
+module.exports = router;
